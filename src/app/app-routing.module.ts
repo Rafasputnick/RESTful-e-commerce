7a128bac490dcdaf694102acdca43ea53eef9a7f
Rafael Lourenço <rafalourenc123641@gmail.com>
2020-10-29 17:27:46 -0300

    Interface+Routing in Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
+// arquivo que cuida das rotas dentro do aplicativo e faz a troca entre componentes
+
+// importa os modulos usados
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// importa os componentes usados no path
+import { MainComponent } from './main-component/app.component';
+import { ProdutosComponent } from './component/produtos/produtos.component';
+// import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+
+// vetor que faz a construção das rotas com param (path e pathMath) url linkada ao componente ideal
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: MainComponent },
+  { path: 'produtos', component: ProdutosComponent},
+  // { path: 'dashboard', component: DashboardComponent },
+  // { path: 'detail/:id', component: HeroDetailComponent },
+  // { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
+// modulos padroes usados
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// modulos do angular materials utilizaados
+import { MatSliderModule } from '@angular/material/slider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+// componentes utilizados
+import { MainComponent } from './main-component/app.component';
+import { ProdutosComponent } from './component/produtos/produtos.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    MainComponent,
+    ProdutosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatCardModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MainComponent]
 })
 export class AppModule { }
